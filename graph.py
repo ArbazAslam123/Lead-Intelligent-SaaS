49ca62ffaa12e053516d617223918cf22c9a5ba8
Arbaz Aslam <aarbazaslam123@gmail.com>
2026-08-18 02:51:46 +0500

    fix: remove unused typer import

--- graph.py
+++ graph.py
@@ -6,7 +6,6 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from pydantic import SecretStr
-from typer import prompt
 from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
 
 
